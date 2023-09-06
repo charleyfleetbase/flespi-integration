f7f93b6772f02bb4711bb1aed8d34072566c13ac
charleyfleetbase <charley@fleetbase.io>
2023-09-06 15:54:58 +0700

    chore: add throw exception

--- src/Providers/FlepsiIntegrationServiceProvider.php
+++ src/Providers/FlepsiIntegrationServiceProvider.php
@@ -4,33 +4,40 @@ namespace Fleetbase\Flespi\Providers;
 
 use Fleetbase\Providers\CoreServiceProvider;
 
+if (!class_exists(CoreServiceProvider::class)) {
+  throw new \Exception('Flespi extension cannot be loaded without `fleetbase/core-api` installed!');
+}
+
+/**
+ * Flespi extension service provider.
+ */
 class FlepsiIntegrationServiceProvider extends CoreServiceProvider
 {
-     /**
-     * Register any application services.
-     *
-     * Within the register method, you should only bind things into the
-     * service container. You should never attempt to register any event
-     * listeners, routes, or any other piece of functionality within the
-     * register method.
-     *
-     * More information on this can be found in the Laravel documentation:
-     * https://laravel.com/docs/8.x/providers
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->register(CoreServiceProvider::class);
-    }
-
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        // Additional setup or operations
-    }
+   * Register any application services.
+   *
+   * Within the register method, you should only bind things into the
+   * service container. You should never attempt to register any event
+   * listeners, routes, or any other piece of functionality within the
+   * register method.
+   *
+   * More information on this can be found in the Laravel documentation:
+   * https://laravel.com/docs/8.x/providers
+   *
+   * @return void
+   */
+  public function register()
+  {
+      $this->app->register(CoreServiceProvider::class);
+  }
+
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+      // Additional setup or operations
+  }
 }
